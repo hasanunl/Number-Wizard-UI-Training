1f74f6e7766e1c25cf0a210b782a07a627de0a87
hasanunl <hu1997tr@gmail.com>
2017-05-30 16:56:33 +0300

    Finished training

---
+++ Assets/levelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelManager : MonoBehaviour {
+
+	public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested."+name);
+        Application.LoadLevel(name);
+    }
+
+    public void quitRequest()
+    {
+        Debug.Log("I want to quit.");
+        Application.Quit();
+    }
+}
